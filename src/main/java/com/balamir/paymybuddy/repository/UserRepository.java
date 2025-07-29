08ebec02dc7a2b4d2eb77058f34963fc246c4798
BalamiRR <fuatkara.95@gmail.com>
2025-07-29 12:07:56 +0200

    Repo config.

---
+++ src/main/java/com/balamir/paymybuddy/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.balamir.paymybuddy.repository;
+
+import com.balamir.paymybuddy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+    void deleteByEmail(String email);
+}

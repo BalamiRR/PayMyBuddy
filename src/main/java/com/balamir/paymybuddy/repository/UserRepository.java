df31bda80042c46efae916f53f2a861e498f59fa
BalamiRR <fuatkara.95@gmail.com>
2025-08-04 13:57:46 +0200

    findByEmail

---
+++ src/main/java/com/balamir/paymybuddy/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.balamir.paymybuddy.repository;
+
+import com.balamir.paymybuddy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+}

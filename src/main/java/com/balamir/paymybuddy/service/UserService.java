56f31eef16a65db669bc4bf147c58d29259145c0
BalamiRR <fuatkara.95@gmail.com>
2025-08-04 14:00:25 +0200

    Service package and class creation

---
+++ src/main/java/com/balamir/paymybuddy/service/UserService.java
@@ -0,0 +1,9 @@
+package com.balamir.paymybuddy.service;
+
+import com.balamir.paymybuddy.model.User;
+import com.balamir.paymybuddy.model.dto.UserDto;
+
+public interface UserService {
+    User findByEmail(String email);
+    void save(UserDto userDto);
+}
---
+++ src/main/java/com/balamir/paymybuddy/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.balamir.paymybuddy.service;
+
+import com.balamir.paymybuddy.model.User;
+import com.balamir.paymybuddy.model.dto.UserDto;
+import com.balamir.paymybuddy.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void save(UserDto userDto) {
+        if(userDto != null){
+            User newUser = new User();
+            newUser.setUserName(userDto.getUserName());
+            newUser.setEmail(userDto.getEmail());
+            newUser.setPassword(userDto.getPassword());
+            userRepository.save(newUser);
+        }
+    }
+
+
+}

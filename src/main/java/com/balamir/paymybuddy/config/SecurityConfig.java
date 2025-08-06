a0b971f6a117dd6310d06e186511750bcd209582
BalamiRR <fuatkara.95@gmail.com>
2025-08-06 15:47:37 +0200

    Security Config for the loginPage

--- src/main/java/com/balamir/paymybuddy/config/SecurityConfig.java
+++ src/main/java/com/balamir/paymybuddy/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.balamir.paymybuddy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -10,5 +12,29 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/signup", "/css/**", "/js/**", "/images/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+
+            .formLogin(form -> form
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/home", true)
+                    .failureUrl("/login?error=true")
+                    .permitAll()
+            )
+
+            .logout(logout -> logout
+                    .logoutSuccessUrl("/login?logout=true")
+                    .permitAll()
+            );
+
+        return http.build();
     }
 }

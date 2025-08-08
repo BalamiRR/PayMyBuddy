6fc6f0e11fcbc4c423f7a13f45253ddd7f06b357
BalamiRR <fuatkara.95@gmail.com>
2025-08-08 18:18:36 +0200

    Added new log in the UserDetails method in CustomUserDetailsService class

--- src/main/java/com/balamir/paymybuddy/config/CustomUserDetailsService.java
+++ src/main/java/com/balamir/paymybuddy/config/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.balamir.paymybuddy.config;
 import com.balamir.paymybuddy.model.User;
 import com.balamir.paymybuddy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,16 +12,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email);
         if(user == null){
+            log.error("User can not be found {} ", email);
             throw new UsernameNotFoundException("User NOT FOUND");
         }
 

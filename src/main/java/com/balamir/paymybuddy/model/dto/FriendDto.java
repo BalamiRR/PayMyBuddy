683cf750395194212070a73bae2ee452a38a7394
BalamiRR <fuatkara.95@gmail.com>
2025-08-14 13:48:39 +0200

    Adding validations

--- src/main/java/com/balamir/paymybuddy/model/dto/FriendDto.java
+++ src/main/java/com/balamir/paymybuddy/model/dto/FriendDto.java
@@ -1,6 +1,9 @@
 package com.balamir.paymybuddy.model.dto;
 
 import com.balamir.paymybuddy.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,6 +12,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FriendDto {
+    @NotNull(message = "User cannot be null")
     private User user;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Size(min = 6, message = "Email must be at least 6 characters long")
     private String friendEmail;
 }

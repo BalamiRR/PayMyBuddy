fbfd27e7298654cb27a7ea30ef4ab12736fe7422
BalamiRR <fuatkara.95@gmail.com>
2025-08-14 13:50:50 +0200

    Adding validations

--- src/main/java/com/balamir/paymybuddy/model/dto/UserDto.java
+++ src/main/java/com/balamir/paymybuddy/model/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.balamir.paymybuddy.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,7 +10,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 6, message = "Username must be at least 6 characters long")
     private String userName;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Size(min = 6, message = "Email must be at least 6 characters long")
     private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 }

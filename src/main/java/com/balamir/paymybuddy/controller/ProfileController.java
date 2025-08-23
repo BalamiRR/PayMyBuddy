ad6994916b622217ac21804b855f0d5771bc7a96
BalamiRR <fuatkara.95@gmail.com>
2025-08-23 18:47:49 +0200

    Showing the username and email as default

--- src/main/java/com/balamir/paymybuddy/controller/ProfileController.java
+++ src/main/java/com/balamir/paymybuddy/controller/ProfileController.java
@@ -1,8 +1,12 @@
 package com.balamir.paymybuddy.controller;
 
+import com.balamir.paymybuddy.model.User;
+import com.balamir.paymybuddy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/profile")
 public class ProfileController {
 
+    private final UserService userService;
+
     @GetMapping
-    public String profilePage(){
+    public String profilePage(Authentication authentication, Model model) {
+        User user = userService.findByEmail(authentication.getName());
+        model.addAttribute("userInfo", user);
         return "profile";
     }
 

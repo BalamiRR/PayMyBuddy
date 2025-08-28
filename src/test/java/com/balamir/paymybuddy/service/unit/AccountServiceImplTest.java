72e423b7d9b7d02bfdf3590f0d80f91263e5c325
BalamiRR <fuatkara.95@gmail.com>
2025-08-28 14:01:19 +0200

    Config

--- src/test/java/com/balamir/paymybuddy/service/unit/AccountServiceImplTest.java
+++ src/test/java/com/balamir/paymybuddy/service/unit/AccountServiceImplTest.java
@@ -10,13 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.math.BigDecimal;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -57,70 +51,5 @@ public class AccountServiceImplTest {
         assertThat(result).isNull();
     }
 
-    @Test
-    void test_addMoney(){
-        User user = new User();
-        user.setId(1);
 
-        Account account = new Account();
-        account.setUser(user);
-        account.setBalance(BigDecimal.ZERO);
-
-        when(accountRepository.findByUserId(1)).thenReturn(account);
-
-        BigDecimal amountToAdd = BigDecimal.valueOf(100);
-        accountServiceImpl.addMoney(1, amountToAdd);
-
-        assertEquals(amountToAdd, account.getBalance());
-        verify(accountRepository).save(account);
-    }
-
-//    @Test
-//    void test_addMoney_NegativeAmount() {
-//        User user = new User();
-//        user.setId(1);
-//
-//        Account account = new Account();
-//        account.setUser(user);
-//        account.setBalance(BigDecimal.ZERO);
-//
-//        when(accountRepository.findByUserId(1)).thenReturn(account);
-//
-//        BigDecimal negativeAmount = BigDecimal.valueOf(-50);
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            accountServiceImpl.addMoney(1, negativeAmount);
-//        });
-//
-//        verify(accountRepository, never()).save(account);
-//    }
-
-    @Test
-    void test_addMoney_AccountNotFound() {
-        when(accountRepository.findByUserId(1)).thenReturn(null);
-
-        BigDecimal amount = BigDecimal.valueOf(50);
-
-        assertThrows(RuntimeException.class, () -> {
-            accountServiceImpl.addMoney(1, amount);
-        });
-    }
-
-    @Test
-    void test_addMoney_NullBalance() {
-        User user = new User();
-        user.setId(1);
-
-        Account account = new Account();
-        account.setUser(user);
-        account.setBalance(null);
-
-        when(accountRepository.findByUserId(1)).thenReturn(account);
-
-        BigDecimal amount = BigDecimal.valueOf(75);
-        accountServiceImpl.addMoney(1, amount);
-
-        assertEquals(amount, account.getBalance());
-        verify(accountRepository).save(account);
-    }
 }

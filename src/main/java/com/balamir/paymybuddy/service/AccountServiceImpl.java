155f8f3585b5b157a256b3de2dd8dabe242633ce
BalamiRR <fuatkara.95@gmail.com>
2025-08-28 14:00:26 +0200

    Deleting addMoney

--- src/main/java/com/balamir/paymybuddy/service/AccountServiceImpl.java
+++ src/main/java/com/balamir/paymybuddy/service/AccountServiceImpl.java
@@ -6,8 +6,6 @@ import com.balamir.paymybuddy.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 @RequiredArgsConstructor
 @Service
@@ -19,25 +17,6 @@ public class AccountServiceImpl implements AccountService{
     @Override
     public Account findByUserId(int id) {
         return accountRepository.findByUserId(id);
-    }
-
-    @Override
-    public void addMoney(int userId, BigDecimal amount) {
-        Account account = accountRepository.findByUserId(userId);
-        if (account == null) {
-            throw new RuntimeException("Account not found for user id: " + userId);
-        }
-
-        if (account.getBalance() == null) {
-            account.setBalance(BigDecimal.ZERO);
-        }
-
-//        if ("USD".equalsIgnoreCase(currency)) {
-//            amount = amount.divide(BigDecimal.valueOf(1.17), 2, RoundingMode.HALF_UP);
-//        }
-
-        account.setBalance(account.getBalance().add(amount));
-        accountRepository.save(account);
     }
 
 }

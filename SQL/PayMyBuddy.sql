5e5d1699aabffa69c2944ca1a65e50fab8a9b3c5
BalamiRR <fuatkara.95@gmail.com>
2025-08-31 19:20:29 +0200

    SQL

--- SQL/PayMyBuddy.sql
+++ SQL/PayMyBuddy.sql
@@ -20,10 +20,11 @@ CREATE TABLE `account` (
   `created_at` DATETIME(6) NOT NULL,
   `updated_at` DATETIME(6) DEFAULT NULL,
   `balance` decimal(19,2) DEFAULT NULL,
-  `user_id` INT DEFAULT NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_account_user_id` (`user_id`),
   CONSTRAINT `fk_account_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `friends`;
@@ -36,8 +37,10 @@ CREATE TABLE `friends` (
   PRIMARY KEY (`id`),
   KEY `fk_friends_friend_id` (`friend_id`),
   KEY `fk_friends_user_id` (`user_id`),
-  CONSTRAINT `fk_friends_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_friends_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+	ON DELETE CASCADE,
   CONSTRAINT `fk_friends_friend_id` FOREIGN KEY (`friend_id`) REFERENCES `user` (`id`)
+	ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `transaction`;
@@ -49,11 +52,19 @@ CREATE TABLE `transaction` (
   `charge` DECIMAL(19,2) DEFAULT NULL,
   `description` VARCHAR(255) DEFAULT NULL,
   `status` ENUM('INITIATED', 'PROCESSING', 'SUCCESS', 'FAILED') DEFAULT 'INITIATED',
-  `sender` INT DEFAULT NULL,
-  `receiver` INT DEFAULT NULL,
+  `sender` INT NOT NULL,
+  `receiver` INT NOT NULL,
   `account_id` INT NOT NULL,
   PRIMARY KEY (`id`),
+  KEY `fk_transaction_sender` (`sender`),
+  KEY `fk_transaction_receiver` (`receiver`),
+  KEY `fk_transaction_account` (`account_id`),
+  CONSTRAINT `fk_transaction_sender` FOREIGN KEY (`sender`) REFERENCES `user` (`id`)
+    ON DELETE CASCADE,
+  CONSTRAINT `fk_transaction_receiver` FOREIGN KEY (`receiver`) REFERENCES `user` (`id`)
+    ON DELETE CASCADE,
   CONSTRAINT `fk_transaction_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- SQL/test_data.sql
+++ SQL/test_data.sql
@@ -7,10 +7,7 @@ USE `payMyBuddy`;
 INSERT INTO `user` VALUES
 	(1, '2020-08-10 11:11:10.311000', '2020-08-10 11:11:10.311000', 'ibrahimovic@gmail.com', '$2a$10$xysGwpZ5dITn6Cp05gM.ZOukR8FKtC0acpfgEWz7JehXxMzNLKLX6', 'ibrahimovic'),
 	(2, '2022-06-12 13:41:30.312000', '2022-06-12 13:41:30.312000', 'cristiano_ronaldo@gmail.com', '$2a$10$NtBprXvh7tazYzjmn0d0I.qVkGexDGHPQGyLCPGW4sAoyRqrC94Ju', 'cristiano_ronaldo'),
-    (3, '2023-09-13 14:21:50.311000', '2023-09-13 14:21:50.311000', 'lionel_messi@gmail.com', '$2a$10$pPngu5hQgs3bBZCdss4m5OcVxpA9fv3UomG0OFY4e7CFfxDnwFtim', 'lionel_messi'),
-	(4, '2020-08-10 11:11:10.311000', '2020-08-10 11:11:10.311000', 'rivaldo@gmail.com', '$2a$10$PEh.x9J1TBHxh2s9hQTjlev8QT8htj8e9zDB04JZpTcb5QQ7hWCa.', 'rivaldo'),
-	(5, '2022-06-12 13:41:30.312000', '2022-06-12 13:41:30.312000', 'haaland@gmail.com', '$2a$10$gUnxvG61SNeSHuZSPQ2xf.x3.m.MLUk0P8s6ITXGOvcdlmY.RqF2K', 'haaland'),
-    (6, '2023-09-13 14:21:50.311000', '2023-09-13 14:21:50.311000', 'fuatkara@gmail.com', '$2a$10$XlIhubJRyuRpYi9Jfs2Gb.fOmaiBAc6Kp93OGrXyH516lmXnqOCzG', 'fuatkara');
+    (3, '2023-09-13 14:21:50.311000', '2023-09-13 14:21:50.311000', 'lionel_messi@gmail.com', '$2a$10$pPngu5hQgs3bBZCdss4m5OcVxpA9fv3UomG0OFY4e7CFfxDnwFtim', 'lionel_messi');
 
 
 SELECT * FROM user;
@@ -20,9 +17,9 @@ SELECT id, email, userName FROM user;
 -- Adding data for Account
 --
 INSERT INTO `account` VALUE
-	(4, '2021-11-03 09:17:13.581000', '2021-11-03 09:17:13.121000', '1050', 1),
-	(5, '2021-11-03 09:17:13.581000', '2021-11-03 09:17:13.121000', '990', 2),
-	(6, '2021-11-03 09:17:13.581000', '2021-11-03 09:17:13.121000', '900', 3);
+	(1, '2021-11-03 09:17:13.581000', '2021-11-03 09:17:13.121000', '1050', 1),
+	(2, '2021-11-03 09:17:13.581000', '2021-11-03 09:17:13.121000', '990', 2),
+	(3, '2021-11-03 09:17:13.581000', '2021-11-03 09:17:13.121000', '0', 3);
 
 SELECT * FROM account;
 
@@ -40,11 +37,8 @@ SELECT * FROM friends;
 -- Adding data for Transaction table
 --
 INSERT INTO `transaction` VALUES
-    (8, '2021-11-03 10:16:14.581000', '2021-11-03 09:17:13.121000', 101.90, 0.01, 'Gift', 'INITIATED', 6, 4, 4),
-    (10, '2021-11-03 10:16:14.781000', '2021-11-03 09:17:13.781000', 10, 0.03, 'Debt', 'SUCCESS', 6, 5, 5),
-    (11, '2021-11-03 10:16:14.651000', '2021-11-03 09:17:13.651000', 200.15, 0.04, 'Gift', 'INITIATED', 4, 5, 6),
-    (12, '2021-11-03 10:16:14.846000', '2021-11-03 09:17:13.846000', 175.38, 0.05, 'OK', 'FAILED', 4, 6, 4),
-    (13, '2021-11-03 10:16:14.652000', '2021-11-03 09:17:13.652000', 150.12, 0.05, 'Debt', 'PROCESSING', 5, 4, 5),
-	(14, '2021-11-03 10:16:14.789000', '2021-11-03 09:17:13.789000', 185, 0.05, 'Gift', 'SUCCESS', 4, 6, 6);
+    (8, '2021-11-03 10:16:14.581000', '2021-11-03 09:17:13.121000', 101.90, 0.01, 'Gift', 'INITIATED', 1, 2, 1),
+    (10, '2021-11-03 10:16:14.781000', '2021-11-03 09:17:13.781000', 10, 0.03, 'Debt', 'SUCCESS', 2, 3, 2),
+    (11, '2021-11-03 10:16:14.651000', '2021-11-03 09:17:13.651000', 200.15, 0.04, 'Gift', 'INITIATED', 1, 3, 1);
 
 SELECT * FROM transaction;

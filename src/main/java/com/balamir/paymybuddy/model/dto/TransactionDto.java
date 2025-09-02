ffbd32ade168ceb2a2b44074f0c7ff8b052bf3e2
BalamiRR <fuatkara.95@gmail.com>
2025-09-02 15:11:43 +0200

    Adding 0.5 transfer fee

--- src/main/java/com/balamir/paymybuddy/model/dto/TransactionDto.java
+++ src/main/java/com/balamir/paymybuddy/model/dto/TransactionDto.java
@@ -4,6 +4,7 @@ import com.balamir.paymybuddy.model.Transaction;
 import lombok.Getter;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Instant;
 
 @Getter
@@ -14,6 +15,7 @@ public class TransactionDto {
     private BigDecimal amount;
     private String description;
     private Instant createdAt;
+    private BigDecimal netAmount;
     private String direction; // OUTGOING and INCOMING arrows
 
     public TransactionDto(Transaction transaction, Integer currentUserId) {
@@ -26,6 +28,11 @@ public class TransactionDto {
         this.amount = transaction.getAmount();
         this.description = transaction.getDescription();
         this.createdAt = transaction.getCreatedAt();
+        BigDecimal feeRate = new BigDecimal("0.005");
+        this.netAmount = transaction.getAmount()
+                .subtract(transaction.getAmount().multiply(feeRate))
+                .setScale(2, RoundingMode.HALF_UP);
+
         this.direction = transaction.getSender().getId().equals(currentUserId) ? "OUTGOING" : "INCOMING";
     }
 }

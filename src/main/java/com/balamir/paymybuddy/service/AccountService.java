0bda75e7d58703b2e0afca355859865c6fdecb5b
BalamiRR <fuatkara.95@gmail.com>
2025-09-09 09:21:07 +0200

    Add money modal

--- src/main/java/com/balamir/paymybuddy/service/AccountService.java
+++ src/main/java/com/balamir/paymybuddy/service/AccountService.java
@@ -2,6 +2,10 @@ package com.balamir.paymybuddy.service;
 
 import com.balamir.paymybuddy.model.Account;
 
+import java.math.BigDecimal;
+
 public interface AccountService {
     Account findByUserId(int id);
+    void addMoney(int userId, BigDecimal amount, String currency);
+
 }
